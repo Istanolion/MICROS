305827321fb8ee5c94e102be907fe69ef4e66471
AmosMVL2261 <refi2261@gmail.com>
2020-03-16 00:38:07 -0600

    Add files via upload
    Estos son tus códigos comentados, el de multiplicación es el único mio, aunque igual esta comentado

---
+++ Tareas Comentadas/Division_comentado.asm
@@ -0,0 +1,38 @@
+dividendo equ H'20'	 		; Registro utilizado para dividendo
+divisor equ H'21'			; Registro utilizado para divisor
+resultado equ H'22'			; Resultado = dividendo / divisor con numeros enteros
+residuo equ H'23'			; Registro utilizado para residuo
+auxiliar equ H'24'			; Registro utilizado para auxiliar
+
+	org 0					; vector de reset
+	goto inicio				; ve al inicio del programa
+ 	org 5					; vector de inicio de programa
+
+inicio: 
+	MOVLW 0x00				; Caragamos con 0 a W para limpiar los registros 
+	MOVWF resultado
+	MOVWF residuo
+	XORWF divisor,w 		; Verificamos que divisor no sea 0 con la compuerta XOR
+	BTFSC STATUS,Z			; Si la bandera Z se levanta como resultado
+	GOTO $					; Termina el programa
+	MOVF dividendo,w 		; Copia el valor del dividendo a W
+	MOVWF auxiliar			; Y lo copiamos a auxiliar
+	MOVF divisor,w 			; Copiamos a divisor en w
+	SUBWF dividendo,w 		; y efectuamos una resta que almacenamos en W
+	BTFSS STATUS,C 			; Comprobamos que no haya sido negativo
+	GOTO $					; Si lo fue termina el programa
+	GOTO division			; Si no lo fue ve a divisor
+division 
+	MOVF divisor,w 			; Copiamos divisor en W
+	SUBWF dividendo,f 		; le restamos al dividendo el divisor
+	BTFSS STATUS,C 			; Comprobamos la operacion con la bandera C
+	GOTO residuo			; si C es cero nos vamos a residuo
+	INCF resultado 			; en caso contrario incrementamos el resultado en 1
+	GOTO division 			; y repetimos la operacion
+residuo: 
+	ADDWF dividendo,w 		; agregamos a w el dividendo
+	MOVWF residuo 			; y lo movemos a residuo
+	MOVF auxiliar,w 		; copiamos el auxiliar en w
+	MOVWF dividendo 		; y lo copiamos en dividendo
+	GOTO $					; terminamos programa
+	END						; Fin de programa
---
+++ Tareas Comentadas/HextoDec_Comentado.asm
@@ -0,0 +1,46 @@
+	processor 16f877		;micro a usar
+	include<p16f877.inc>	;libreria para ocupar nombres de registros
+	
+rega equ H'20'				; numero en HEX
+decH equ H'21'				; Registro parte alta
+decL equ H'22'				; Registro
+regAux equ H'23'			; Resultado de la multiplicacion
+
+	org 0					; Vector de reset
+	goto inicio				; Ve al inicio del programa
+ 	org 5
+
+inicio: 
+	MOVLW 0x00				; Cargamos el valor 0 en W
+	MOVWF decH				; Limpiamos el registro
+	MOVWF decL				; Limpiamos el registro
+	MOVF rega,w 			; copiamos rega en W
+	MOVWF regAux 			; copiamos w en regAux
+	GOTO centena 			; Ve a centena
+centena: 
+	MOVLW 0x64 				; Copiamos el valor de 64 en w
+	SUBWF regAux,w 			; Realizamos la resta con regAux y la almacenamos en w
+	BTFSS STATUS,C 			; Comprobamos el estado de la bandera C de STATUS
+	GOTO decena 			; Si esta en 0 vamos a decena
+	INCF decH 				; Si esta en 1 incrementamos decH
+	MOVWF regAux 			; Copiamos W en regAux
+	GOTO centena 			; Ve a centena
+decena: 
+	MOVLW 0x0A 				; Copiamos el valor de 0A en W
+	SUBWF regAux,w 			; Realizamos la resta con regAux y la almacenamos en W
+	BTFSS STATUS,C 			; Comprobamos el estado de la bandera C de STATUS
+	GOTO swap 				; Si esta en 0 vamos a swap
+	INCF decL 				; Si esta en 1 incrementamos decL
+	MOVWF regAux 			; Copiamos W en regAux
+	GOTO decena 			; Y repetimos procedimiento
+swap: 
+	SWAPF decL 				; Realiza un intercambio de parte alta con parte baja
+unidad: 
+	MOVLW 0x01				; Copiamos el valor de 01 en W
+	SUBWF regAux,w 			; Realizamos la resta con regAux y almacenamos en w el resultado
+	BTFSS STATUS,C 			; Comprobamos el estado de la bandera C de STATUS
+	GOTO $  				; Si es 0 terminamos programa
+	INCF decL 				; Si es 1 Incrementamos decL
+	MOVWF regAux 			; Copiamos w en regAux
+	GOTO unidad				; Repetimos procedimiento
+	END 					; Fin programa
