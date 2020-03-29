c6462a0ae31a478a9b98dd286c631cf6b3fe8a11
Istanolion <akuma_diego@hotmail.com>
2020-03-29 16:18:33 -0600

    Proyecto2 DONE

--- Proyecto2(LCD)/Proyecto_2.asm
+++ Proyecto2(LCD)/Proyecto_2.asm
@@ -29,6 +29,9 @@ valor12 equ h'32'
 cte10 equ 20h
 cte11 equ 35h
 cte12 equ 61h
+;Auxiliar register for the keypad
+RegAux equ 0x33
+RegAux2 equ 0x34
 
 ;PortA is used to read the deepswitches
 ;PORTB is used to Read the KeyPad
@@ -51,11 +54,11 @@ inicio 						; Etiqueta de inicio de programa
  	BCF STATUS,6 			; Ponemos un 0 en el bit 6 de STATUS (RP1) para cambiar de banco 0 al 1
 	MOVLW H'06'				; W <- h'06'
 	MOVWF ADCON1			; ADCON1 <- (W) desactivar convertidor
-	MOVLW H'3F'				; W <- h'3F'
+	MOVLW H'07'				; W <- h'07' B'00000111'
 	MOVWF TRISA 			; TRISA <- (W) configuramos PORTA como entrada
-	MOVLW H'F0'				; W <- h'F0' B'11110000' We need in and outputs
+	MOVLW H'F0'				; W <- h'F0' B'11110000' We need inputs and outputs. high is input_low is output
 	MOVWF TRISB				; TRISB <- (W) configuramos PORTB como entrada
-	MOVLW H'F8'				; W <- h'F8' B'11111000' Only need three outputs
+	MOVLW H'F8'				; W <- h'F8' B'11111000' WE NEEED THRRE OUTPUTS
 	MOVWF TRISC				; TRISC <- (W) configuramos PORTC como salida
 	MOVLW H'00'				; W <- h'00'
 	MOVWF TRISD				; TRISD <- (W) configuramos PORTD como salida
@@ -67,32 +70,231 @@ Comportamiento:
 loop:
 	MOVLW 0x01
 	call LCD_Comando
-
+	MOVLW 0x02
+	call LCD_Comando
+	CLRF RegAux
+	MOVLW 0x02
+	MOVWF RegAux2
 	MOVF PORTA,W			; W <- (PORTA) leer entrada en PORTA
 	ANDLW H'07'				; Se realiza un AND logico con H'07' (Mascara de bits)
 	ADDWF PCL,F				; Se agrega al PC el valor resultante de aplicar la mascara
-	goto HolaMundo			; Llama la subrutina Hola Mundo
-	goto Names				; Llama la subrutina que imprime los nombres
-	goto Hexadecimal
-	goto Binario
-	goto Deximal
-	goto puma				; Llama la subrutina que imprime el puma
+	goto HolaMundo			; Llama la subrutina Hola Mundo 0
+	goto Names				; Llama la subrutina que imprime los nombres 1
+	goto Hexadecimal		; 2
+	goto Binario			;3
+	goto Decimal			;4
+	goto puma				; Llama la subrutina que imprime el puma 5
 
 	goto loop
 	goto loop
-	GOTO $					; fin del programa
+
+
+Hexadecimal:
+	MOVLW 0x48;
+	CALL LCD_Datos
+	MOVLW 0x27;
+	CALL LCD_Datos
+	CALL ReadKeypad			;WE Read the units, W HAS THE VALUE READ BY THE KEYPAD
+	GOTO loop
+Binario:
+	MOVLW 0x48;
+	CALL LCD_Datos
+	MOVLW 0x27;
+	CALL LCD_Datos
+	CALL ReadKeypad			;WE Read the units, W HAS THE VALUE READ BY THE KEYPAD
+	MOVLW 0xC0	; Dir that starts line 2
+	CALL LCD_Comando
+	MOVLW 0x42;
+	CALL LCD_Datos
+	MOVLW 0x27;
+	CALL LCD_Datos
+	CALL ConversionBinario
+	GOTO loop
+
+Decimal
+	MOVLW 0x48;
+	CALL LCD_Datos
+	MOVLW 0x27;
+	CALL LCD_Datos
+	CALL ReadKeypad			;WE Read the units, W HAS THE VALUE READ BY THE KEYPAD
+	MOVLW 0xC0	; Dir that starts line 2
+	CALL LCD_Comando
+	MOVLW 0x44;
+	CALL LCD_Datos
+	MOVLW 0x27;
+	CALL LCD_Datos
+	CALL ConversionDecimal	
+	GOTO loop
+
+;==========================================================================
+ConversionDecimal:
+centena: 
+	MOVLW 0x64
+	SUBWF RegAux,w
+	BTFSS STATUS,C
+	GOTO Indecena
+	INCF RegAux2
+	MOVWF RegAux
+	GOTO centena
+Indecena:
+	MOVF RegAux2,w
+	CALL LCD_Digito
+	CLRF RegAux2
+decena:  
+	MOVLW 0x0A
+	SUBWF RegAux,w
+	BTFSS STATUS,C
+	GOTO swap
+	INCF RegAux2
+	MOVWF RegAux
+	GOTO decena
+swap: MOVF RegAux2,w
+	CALL LCD_Digito
+	CLRF RegAux2
+unidad: MOVLW 0x01
+	SUBWF RegAux,w
+	BTFSS STATUS,C
+	GOTO finDecimal
+	INCF RegAux2
+	MOVWF RegAux
+	GOTO unidad
+finDecimal
+	MOVF RegAux2,w
+	CALL LCD_Digito
+	RETURN
+;==========================================================================
+ConversionBinario:
+	MOVLW 0x80
+	CALL RESTA
+	MOVLW 0x40
+	CALL RESTA
+	MOVLW 0x20
+	CALL RESTA
+	MOVLW 0x10
+	CALL RESTA
+
+	MOVLW 0x08
+	CALL RESTA
+	MOVLW 0x04
+	CALL RESTA
+	MOVLW 0x02
+	CALL RESTA
+	MOVLW 0x01
+	CALL RESTA
+	RETURN
+RESTA:
+	SUBWF RegAux,w  ;RegAux-w ->W c=1 positivo
+	BTFSS STATUS,C
+	GOTO Imprimir0;Entra si c=0 ie negativo  PRINT 0
+	MOVWF RegAux   ;JUMP SI C=1 POSITIVE
+	MOVLW 0x01
+	CALL LCD_Digito
+	RETURN
+Imprimir0:
+	MOVLW 0x00
+	GOTO LCD_Digito
 	
+;===========================================================================
+Digito:
+	SWAPF RegAux
+	ADDWF RegAux,F
+	CALL LCD_Digito
+	DECFSZ RegAux2
+	GOTO ReadKeypad
+	CALL Retardo_1_Segundo
+	RETURN
+Letra:
+	SWAPF RegAux
+	ADDWF RegAux,F
+	CALL LCD_Letra
+	DECFSZ RegAux2
+	GOTO ReadKeypad
+	CALL Retardo_1_Segundo
+	RETURN
+LCD_Digito:
+	ADDLW 0x30			;W+30, now W has the value needed for the LCD to Display the correct number
+	CALL LCD_Datos		;The display shows the value read by the keypad
+	RETURN
+LCD_Letra:	 
+	ADDLW 0x37			;w+37, now w Has the value needed for the LCD to Display the correct Letter
+	CALL LCD_Datos
+	RETURN
+;===========================================================================
+ReadKeypad:
+	BCF PORTB,3
+	BSF PORTB,0 	;ROW 7-A
+	MOVLW 0x07
+	BTFSC PORTB,4   ;IF PORTB,4=1 THEN THE NUMBER IS 7
+	GOTO Digito		;NUMBER 7
+	
+	MOVLW 0x08
+	BTFSC PORTB,5   ;IF PORTB,5=1 THEN THE NUMBER IS 8
+	GOTO Digito		;NUMBER 8
 
+	MOVLW 0x09
+	BTFSC PORTB,6   ;IF PORTB,6=1 THEN THE NUMBER IS 9
+	GOTO Digito		;NUMBER 9
+	
+	MOVLW 0x0A
+	BTFSC PORTB,7   ;IF PORTB,7=1 THEN THE NUMBER IS A
+	GOTO Letra		;NUMBER A
 
-Hexadecimal
-	goto loop
+	BCF PORTB,0
+	BSF PORTB,1 	;ROW 4,5,6,B
+	MOVLW 0x04
+	BTFSC PORTB,4   ;IF PORTB,4=1 THEN THE NUMBER IS 7
+	GOTO Digito		;NUMBER 7
+	
+	MOVLW 0x05
+	BTFSC PORTB,5   ;IF PORTB,5=1 THEN THE NUMBER IS 8
+	GOTO Digito		;NUMBER 8
 
-Binario
-	goto loop
+	MOVLW 0x06
+	BTFSC PORTB,6   ;IF PORTB,6=1 THEN THE NUMBER IS 9
+	GOTO Digito		;NUMBER 9
+	
+	MOVLW 0x0B
+	BTFSC PORTB,7   ;IF PORTB,7=1 THEN THE NUMBER IS A
+	GOTO Letra		;NUMBER A
+	
+	BCF PORTB,1
+	BSF PORTB,2 	;ROW 4,5,6,B
+	MOVLW 0x01
+	BTFSC PORTB,4   ;IF PORTB,4=1 THEN THE NUMBER IS 7
+	GOTO Digito		;NUMBER 7
+	
+	MOVLW 0x02
+	BTFSC PORTB,5   ;IF PORTB,5=1 THEN THE NUMBER IS 8
+	GOTO Digito		;NUMBER 8
 
-Deximal
-	goto loop
+	MOVLW 0x03
+	BTFSC PORTB,6   ;IF PORTB,6=1 THEN THE NUMBER IS 9
+	GOTO Digito		;NUMBER 9
+	
+	MOVLW 0x0C
+	BTFSC PORTB,7   ;IF PORTB,7=1 THEN THE NUMBER IS A
+	GOTO Letra		;NUMBER A
 
+	BCF PORTB,2
+	BSF PORTB,3 	;ROW 4,5,6,B
+	MOVLW 0x0F
+	BTFSC PORTB,4   ;IF PORTB,4=1 THEN THE NUMBER IS 7
+	GOTO Letra		;NUMBER 7
+	
+	MOVLW 0x00
+	BTFSC PORTB,5   ;IF PORTB,5=1 THEN THE NUMBER IS 8
+	GOTO Digito		;NUMBER 8
+
+	MOVLW 0x0E
+	BTFSC PORTB,6   ;IF PORTB,6=1 THEN THE NUMBER IS 9
+	GOTO Letra		;NUMBER 9
+	
+	MOVLW 0x0D
+	BTFSC PORTB,7   ;IF PORTB,7=1 THEN THE NUMBER IS A
+	GOTO Letra		;NUMBER A
+
+	GOTO ReadKeypad
+;===========================================================================
 HolaMundo:
 	MOVLW 0x48
 	CALL LCD_Datos 
@@ -115,7 +317,7 @@ HolaMundo:
 	MOVLW 0x6F
 	CALL LCD_Datos
 	goto loop
-
+;================================================================================
 puma:
 	MOVLW 0x01
 	CALL LCD_Datos
@@ -139,7 +341,7 @@ moveDisplay:
 	MOVLW 0X18				;Move display to Left
 	CALL LCD_Comando
 	Return
-
+;=============================================================================
 createSymbols:
 	MOVLW 0X40
 	CALL LCD_Comando
@@ -257,7 +459,7 @@ createSymbols:
 	MOVLW H'80'         				
 	CALL LCD_Comando
 	RETURN				
-
+;=========================================================================================
 Names:
 ;Nombre de Amos Manuel Vega Lopez
 	MOVLW 0x41
@@ -373,6 +575,21 @@ Names:
 	CALL LCD_Datos
 	
 	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+	CALL moveDisplay
+
 	goto loop
 
 ; ==============================================================================
